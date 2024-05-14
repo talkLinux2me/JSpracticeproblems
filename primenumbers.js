a51fb855f5e3263db50e4fcdecf2611ea67a319d
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-05-14 14:11:15 -0400

    PrimeNumberInstructions

--- primenumbers.js
+++ primenumbers.js
@@ -1,3 +1,7 @@
+/**Declare an arbitrary number, n.
+Create a loop that searches for the next prime number, starting at n and incrementing from there.
+As soon as you find the prime number, log that number and exit the loop. */
+
 function checkPrime(start) {
     let i = 2;
 
